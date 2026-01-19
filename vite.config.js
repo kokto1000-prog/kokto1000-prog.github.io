2e4d929d9e084320241cdb5d3629fa4b2036cd31
kokto1000-prog <kokto1000@gmail.com>
2026-01-20 01:53:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<Wallet>/', // Svarīgi: slīpsvītras sākumā un beigās
+  base: '/<kokto1000-prog.github.io>/', // Svarīgi: slīpsvītras sākumā un beigās
   plugins: [
     react(),
     VitePWA({
